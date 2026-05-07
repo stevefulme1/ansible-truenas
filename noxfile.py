aafca29b12b0d06106fd54f39de8372d981d7069
sfulmer <sfulmer@redhat.com>
2026-05-07 10:41:36 -0400

    Fix tox and nox to run ansible-test from proper collection path
    Both tools need to run ansible-test from inside the
    ansible_collections/truenas/storage directory hierarchy for sanity
    tests to recognize modules correctly.

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,23 @@
 import nox
+import os
+import shutil
 
 nox.options.sessions = ["lint", "sanity"]
+
+COLLECTION_PATH = os.path.join(
+    os.path.expanduser("~"),
+    ".ansible", "collections", "ansible_collections", "truenas", "storage",
+)
+
+
+def _install_collection(session):
+    if os.path.exists(COLLECTION_PATH):
+        shutil.rmtree(COLLECTION_PATH)
+    shutil.copytree(
+        ".",
+        COLLECTION_PATH,
+        ignore=shutil.ignore_patterns(".nox", "__pycache__", "*.egg-info"),
+    )
 
 
 @nox.session(python=["3.12", "3.13"])
@@ -12,9 +29,11 @@ def lint(session):
 @nox.session(python=["3.12", "3.13"])
 def sanity(session):
     session.install("ansible-core>=2.16")
+    _install_collection(session)
+    session.chdir(COLLECTION_PATH)
     session.run(
         "ansible-test", "sanity", "--local", "-v", "--color",
-        "--python", f"{session.python}",
+        "--python", "default",
     )
 
 
